974fb30caa1031c4a2ad38ff036c77f433787719
b02901073 <you@example.com>
2017-03-27 14:17:35 +0800

    3/27

--- chat.js
+++ chat.js
@@ -9,64 +9,23 @@ app.get('/', function(req, res) {
     res.sendfile('chat.html');
 });
 
-
-
 http.listen(3000, function() {
     console.log('listening on *:3000');
 });
 
 io.on('connection', function(socket) {
     console.log("a users connect");
-    for (var i = 1; i < messages.length; i++) {
-        console.log(i);
-        var now = i;
-        while (likes[now] > likes[now - 1] && now > 0) {
-            console.log("change");
-            var tempmes = messages[now];
-            var templike = likes[now];
-            likes[now] = likes[now - 1];
-            messages[now] = messages[now - 1];
-            likes[now - 1] = templike;
-            messages[now - 1] = tempmes;
-            now--;
-        }
-
-    }
-
+    io.emit('broadcast','Welcome to Ping Chat');
     for (var i = 0; i < messages.length; i++) {
         socket.emit('chat message', messages[i]);
     }
 
-    for (var i = 0; i < messages.length; i++) {
-        var count = likes[i];
-        while (count > 0) {
-            socket.emit('like', i);
-            count--;
-        }
-    }
-
-
     socket.on('chat message', function(msg) {
         io.emit('chat message', msg);
         messages.push(msg);
-        likes.push(0);
-    });
-
-    socket.on('like', function(aaa) {
-        io.emit('like', aaa);
-        //console.log(aaa);
-        likes[aaa]++;
     });
 
     socket.on('disconnect', function() {
         console.log('user disconnected');
     });
-    /*
-       socket.on('change', function(change){
-        io.emit('change' , change);
-      });
-
-       socket.on('switch', function(bbb){
-        io.emit('switch' , bbb);
-      });*/
 });
